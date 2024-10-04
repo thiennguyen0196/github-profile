f35b8dd4a1ef990068eec83ba2c288c125409d44
Thiên Nguyễn <hoangthien0196@gmail.com>
2024-10-04 07:23:28 +0700

    #4 Integrate user list

--- app/src/main/java/co/thiennguyen/github_profile/ui/ErrorMapping.kt
+++ app/src/main/java/co/thiennguyen/github_profile/ui/ErrorMapping.kt
@@ -3,11 +3,13 @@ package co.thiennguyen.github_profile.ui
 import android.content.Context
 import co.thiennguyen.github_profile.R
 import co.thiennguyen.github_profile.domain.exceptions.ApiException
+import co.thiennguyen.github_profile.domain.exceptions.NoConnectivityException
 import co.thiennguyen.github_profile.extensions.showToast
 
 fun Throwable.userReadableMessage(context: Context): String {
     return when (this) {
         is ApiException -> error?.message
+        is NoConnectivityException -> context.getString(R.string.error_no_connectivity)
         else -> message
     } ?: context.getString(R.string.error_generic)
 }
--- app/src/main/java/co/thiennguyen/github_profile/ui/models/UserUiModel.kt
+++ app/src/main/java/co/thiennguyen/github_profile/ui/models/UserUiModel.kt
@@ -1,7 +1,17 @@
 package co.thiennguyen.github_profile.ui.models
 
+import co.thiennguyen.github_profile.domain.models.User
+
 data class UserUiModel(
     val loginUserName: String,
     val avatarUrl: String,
     val htmlUrl: String,
 )
+
+fun User.toUiModel() = UserUiModel(
+    loginUserName = this.login,
+    avatarUrl = this.avatarUrl,
+    htmlUrl = this.htmlUrl,
+)
+
+fun List<User>.toUiModels() = this.map { it.toUiModel() }
